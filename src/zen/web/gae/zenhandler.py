2a5f78c869b353c171f054df1a5341dbadb174a4
renzon <renzon@gmail.com>
2012-08-17 11:12:49 -0300

    Close #8

---
+++ src/zen/web/gae/zenhandler.py
@@ -0,0 +1,40 @@
+
+
+class ZenHandler(object):
+    def __init__(self,handler=None):
+        if handler:
+            self.handler=handler
+
+    @property
+    def handler(self):
+        return self._handler
+
+    @handler.setter
+    def handler(self,value):
+        self._handler=value
+        self.request=value.request
+        self.response=value.response
+
+
+    def get(self,param,default_value=""):
+        values=self.request.get_all(param)
+        if not values: return default_value
+        if len(values)==1: return values[0]
+        return values
+
+
+    def write(self,content):
+        return self.response.write(content)
+
+    def render(self,tmpl_name,values={}):
+        return self.tmpl.render(tmpl_name,values)
+
+    def write_tmpl(self,tmpl_name,values={}):
+        return self.write(self.render(tmpl_name,values))
+
+
+
+
+
+
+
