b289dd552b5509cf277a95f72a0ea853a92b6500
renzon <renzon@gmail.com>
2014-06-26 12:52:20 -0300

    close #10

--- project_template/backend/src/web/rest.py
+++ project_template/backend/src/web/rest.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from tekton.gae.middleware.json_middleware import JsonResponse
+from tekton.gae.middleware.json_middleware import JsonResponse, JsonUnsecureResponse
 
 
 def items():
-    JsonResponse({'data': [1, 2, 3]})
+    return JsonResponse({'data': [1, 2, 3]})
+
+def unsecure():
+    return JsonUnsecureResponse({'data': [1, 2, 3]})
