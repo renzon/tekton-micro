95d939cea0d983da35d35c6c90d5de2ae137873f
renzon <renzon@gmail.com>
2014-06-26 15:24:26 -0300

    close #13 email dependencies cleaned

--- src/tekton/gae/middleware/webapp2_dependencies.py
+++ src/tekton/gae/middleware/webapp2_dependencies.py
@@ -3,14 +3,6 @@ from __future__ import absolute_import, unicode_literals
 from tekton.gae.middleware import Middleware
 
 
-def execute(next_process, handler, dependencies, **kwargs):
-    dependencies["_req"] = handler.request
-    dependencies["_resp"] = handler.response
-    dependencies["_handler"] = handler
-    dependencies["_dependencies"] = dependencies
-    next_process(dependencies, **kwargs)
-
-
 class Webapp2Dependencies(Middleware):
     def set_up(self):
         self.dependencies["_req"] = self.handler.request
